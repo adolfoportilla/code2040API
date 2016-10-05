62beda1a2a89e9bec02658cb526f85872a00d847
adolfoportilla <adolfoportilla@dhcp-10-202-119-157.tamulink.tamu.edu>
2016-10-05 09:28:59 -0500

    Finish the 5 steps

--- register.py
+++ register.py
@@ -79,10 +79,6 @@ def part4():
 
 
 '''PART 5'''
-
-
-    
-
 def secondConversion(interval):
 
     seconds = 0
@@ -107,38 +103,47 @@ def secondConversion(interval):
 
 def addSeconds(listTimes, datestamp):
 
-    #2016-11-03T18:52:07Z
-    print(listTimes)
-    print(datestamp)
     newTime = ""
-
     carry = 0
-
-
-    #ALMOST DONE
-
-    #ONLY NEED TO CHECK WHEN YOU ADD TWO TIMES, I.E. 59 SEC AND 3 SEC, NEED TO MAKE IT PRINT 02 SEC INSTEAD OF 2
-
-
 
     #Add the seconds, check first if the seconds exceed 60
     if (int(datestamp[17:19]) + listTimes[0]) > 60:
-        newTime = str((int(datestamp[17:19]) + listTimes[0]) % 60) + datestamp[-1]
 
+        #check if the seconds are less than 9, so you can add the 0 to the front of the string
+        if ((int(datestamp[17:19]) + carry + listTimes[0])%60) < 9:
+            newTime = "0" + str((int(datestamp[17:19]) + listTimes[0]) % 60) + datestamp[-1]
+        else:
+            newTime = str((int(datestamp[17:19]) + listTimes[0]) % 60) + datestamp[-1]
         carry = 1
+
+    elif (int(datestamp[17:19]) + listTimes[0]) == 60:
+        newTime = "00" + datestamp[-1]
+        carry = 1
+
     else:
 
         if (int(datestamp[17:19])+carry+listTimes[0]) < 9:
             newTime = datestamp[17] + str((int(datestamp[17:19]) + carry + listTimes[0])) + datestamp[-1] + newTime
-            
         else:
             newTime = str(int(datestamp[17:19]) + listTimes[0]) + datestamp[-1]
+        carry = 0
 
-        
 
     #Add the minutes, check first if the minutes exceed 60
     if (int(datestamp[14:16]) + listTimes[1] + carry ) > 60:
-        newTime = str((int(datestamp[14:16]) + 1 + listTimes[1]) % 60) + datestamp[-4] + newTime
+
+        #check if the miniutes are less than 9, so you can add the 0 to the front of the string
+        if ((int(datestamp[14:16]) + carry + listTimes[1]) % 60) < 10 :
+            newTime = "0" +  str((int(datestamp[14:16]) + carry + listTimes[1]) % 60) + datestamp[-4] + newTime
+        else:
+            newTime = str((int(datestamp[14:16]) + carry + listTimes[1]) % 60) + datestamp[-4] + newTime
+
+        #add a carry for the next level
+        carry = 1
+    elif (int(datestamp[14:16]) + listTimes[1] + carry ) == 60:
+        newTime = "00" + datestamp[-4] + newTime
+        carry = 1
+
     else:
 
         if (int(datestamp[14:16])+carry+listTimes[1]) < 9:
@@ -151,7 +156,20 @@ def addSeconds(listTimes, datestamp):
 
     #Add the hours, check first if the hours exceed 24
     if (int(datestamp[11:13]) + listTimes[2] + carry ) > 24:
-        newTime = str((int(datestamp[11:13]) + 1 + listTimes[2]) % 24) + datestamp[-7] + newTime
+        
+        if ((int(datestamp[11:13]) + carry + listTimes[2]) % 24 ) < 10:
+            newTime = "0" +  str((int(datestamp[11:13]) + carry + listTimes[2]) % 24) + datestamp[-7] + newTime
+        else:
+            newTime = str((int(datestamp[11:13]) + carry + listTimes[2]) % 24) + datestamp[-7] + newTime
+
+        #add a carry for the next level
+        carry = 1
+
+    #exact hour adds to 24
+    elif (int(datestamp[11:13]) + listTimes[2] + carry ) ==  24:
+        newTime = "00"+ datestamp[-7] + newTime
+        carry = 1
+
     else:
 
         if (int(datestamp[11:13])+carry+listTimes[2]) < 9:
@@ -164,20 +182,50 @@ def addSeconds(listTimes, datestamp):
 
     #Add the days, check first if the days exceed 28, to check feb
     if (int(datestamp[8:10]) + listTimes[3] + carry ) > 28 and int(datestamp[5:7]) == 2:
-        newTime = str((int(datestamp[8:10]) + 1 + listTimes[3]) % 28) + datestamp[-10] + newTime
+        
+        if ((int(datestamp[8:10])+carry+listTimes[3])%28) < 10:
+            newTime = "0"+ str((int(datestamp[8:10]) + carry + listTimes[3]) % 28) + datestamp[-10] + newTime
+        else:
+            newTime = str((int(datestamp[8:10]) + carry + listTimes[3]) % 28) + datestamp[-10] + newTime
+
+        
         newTime = datestamp[0:5] + "03-" + newTime
 
     #Months with 30 days
     elif (int(datestamp[8:10]) + listTimes[3] + carry ) > 30 and (int(datestamp[5:7]) in [4,6,9,11]):
-        newTime = str((int(datestamp[8:10]) + 1 + listTimes[3]) % 30) + datestamp[-10] + newTime
+
+        #Special case when its the end of the month, i.e, 11.30 and you add one day, needs to say 12.01
+        if ((int(datestamp[8:10])+carry+listTimes[3])%30) < 10:
+            newTime = "0"+ str((int(datestamp[8:10]) + carry + listTimes[3])%30) + datestamp[-10] + newTime
+        else:
+            newTime = str((int(datestamp[8:10]) + carry + listTimes[3]) % 30) + datestamp[-10] + newTime
+        
         newTime = datestamp[0:5] + datestamp[5] + str(int(datestamp[6])+1) + "-" + newTime
     
-    elif (int(datestamp[8:10]) + listTimes[3] + carry ) > 31 and (int(datestamp[5:7]) in [12]):
-        newTime = str((int(datestamp[8:10]) + 1 + listTimes[3]) % 31) + datestamp[-10] + newTime
-        newTime = str(int(datestamp[0:5])+1) + "01-" + newTime
+    #Months with 31 days
+    elif (int(datestamp[8:10]) + listTimes[3] + carry ) > 31 and (int(datestamp[5:7]) in [1,3,5,7,8,10]):
 
+        #Special case when its the end of the month, i.e, 11.30 and you add one day, needs to say 12.01
+        if ((int(datestamp[8:10])+carry+listTimes[3])%31) < 9:
+            newTime = "0"+ str((int(datestamp[8:10]) + carry + listTimes[3])%31) + datestamp[-10] + newTime
+        else:
+            newTime = str((int(datestamp[8:10]) + carry + listTimes[3]) % 31) + datestamp[-10] + newTime
+        
+        newTime = datestamp[0:5] + datestamp[5] + str(int(datestamp[6])+1) + "-" + newTime
+
+
+    elif (int(datestamp[8:10]) + listTimes[3] + carry ) > 31 and (int(datestamp[5:7]) in [12]):
+
+        #Special case when its the end of the month, i.e, 11.30 and you add one day, needs to say 12.01
+        if ((int(datestamp[8:10])+carry+listTimes[3])%31) < 9:
+            newTime = "0" + str((int(datestamp[8:10]) + carry + listTimes[3]) % 31) + datestamp[-10] + newTime
+        else:
+            newTime = str((int(datestamp[8:10]) + carry + listTimes[3]) % 31) + datestamp[-10] + newTime
+
+        newTime = str(int(datestamp[0:4])+1) + "-01-" + newTime
+   
     else:
-        if (int(datestamp[8:10])+carry+listTimes[3]) < 9:
+        if ((int(datestamp[8:10])+carry+listTimes[3])%31) < 10:
             newTime = datestamp[8] + str((int(datestamp[8:10]) + carry + listTimes[3])) + datestamp[-10] + newTime
             
         else:
@@ -185,16 +233,9 @@ def addSeconds(listTimes, datestamp):
         newTime = datestamp[0:8] + newTime
 
 
-    #in this case my code did not work
-    #[31, 49, 15, 0]
-    #2016-11-06T05:28:09Z
-    #2016-11- 6T20:17:40Z
-
-
-    print(newTime)
     return newTime
 
-def main():
+def PART5():
     
     url = 'http://challenge.code2040.org/api/dating'
     token = {'token': '57cc8bcb05af9f30e91e56b911abd698'}
@@ -204,13 +245,19 @@ def main():
     datestamp = dictionary.get('datestamp')
     interval = dictionary.get('interval')
 
-    print(datestamp)
-    print(interval)
-
     conversions = secondConversion(interval)
-    timeConverted = addSeconds(conversions, datestamp)
 
-main()
+    #conversions = [8, 35, 15, 0]
+    #datestamp = '2016-10-08T18:41:12Z'
+
+    newDatestamp = addSeconds(conversions, datestamp)
+
+    dictionarySent = {'token': '57cc8bcb05af9f30e91e56b911abd698', 'datestamp': newDatestamp}
+    response2 = requests.post('http://challenge.code2040.org/api/dating/validate', json = dictionarySent)
+
+    print (response2.text)
+
+PART5()
 
 
 
